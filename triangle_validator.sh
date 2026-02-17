f24ea602d873a2479746f33da5e73b2c70df636e
Bugra Ayten <bugraayten@Bugras-MacBook-Air.local>
2026-02-17 03:19:53 +0100

    v1.0

---
+++ triangle_validator.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "Hello, this program is checking that if your triangle is valid or not"
+echo "Please input your side lengths."
+echo "A?"
+read a
+echo "B?"
+read b
+echo "C?"
+read c
+
+while [[ ! $a =~ ^[0-9] ]]
+do
+	echo "'A' is unvalid. Please enter a valid number for A:"
+	read a
+done
+
+while [[ ! $b =~ ^[0-9] ]]
+do
+        echo "'B' is unvalid. Please enter a valid number for B:"
+        read b
+done
+
+while [[ ! $c =~ ^[0-9] ]]
+do
+        echo "'C' is unvalid. Please enter a valid number for C:"
+        read c
+done
+
+echo "Your values are a=$a, b=$b, c=$c."
+
+if [[ $(($a+$b)) -gt $c && $(($a+$c)) -gt $b && $(($b+$c)) -gt $a ]]
+then
+	echo "This is a valid triangle."
+else
+	echo "This is NOT a valid triangle."
+fi
